de5a8d9a2e7f675e4a1e2eb7c8496129955bfc92
DinisAmado <dinisrafaelamado@gmail.com>
2024-06-11 02:36:00 +0100

    codigo finalizado

--- analise.py
+++ analise.py
@@ -1,56 +1,57 @@
 from protocolo_teste import *
-from sklearn.model_selection import learning_curve
 import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
+import pandas as pd
+from sklearn.model_selection import learning_curve
 
 df = pd.read_csv('normalized.csv')
 
-# Análise Gráfica dos Resultados
+# Análise Gráfica dos Resultados:
 
-# Residuals for Linear Regression
-residuals_lr = y_test - y_pred_lr
+# Resíduos para regressão linear
+residuals_lr = YTest - PrevYRl
 
 plt.figure(figsize=(10, 6))
-sns.scatterplot(x=y_pred_lr, y=residuals_lr)
+sns.scatterplot(x=PrevYRl, y=residuals_lr)
 plt.axhline(y=0, color='r', linestyle='--')
 plt.xlabel('Predicted Values')
 plt.ylabel('Residuals')
 plt.title('Residuals vs Predicted Values (Linear Regression)')
 plt.show()
 
-# Feature Importance from Random Forest
-feature_importances = pd.DataFrame(rf_model.feature_importances_, index=x_train.columns,
+# Importância do recurso da Random Forest
+importance = pd.DataFrame(ModeloRf.feature_importances_,
                                    columns=['importance']).sort_values('importance', ascending=False)
 
 plt.figure(figsize=(10, 6))
-sns.barplot(x=feature_importances['importance'], y=feature_importances.index)
+sns.barplot(x=importance['importance'], y=importance.index)
 plt.title("Feature Importances in Random Forest")
 plt.xlabel("Importance")
 plt.ylabel("Feature")
 plt.show()
 
-# Residuals for Random Forest
-residuals_rf = y_test - y_pred_rf
+# Resíduos para Floresta Aleatória
+residuals_rf = YTest - PrevYRl
 
 plt.figure(figsize=(10, 6))
-sns.scatterplot(x=y_pred_rf, y=residuals_rf)
+sns.scatterplot(x=PrevYRl, y=residuals_rf)
 plt.axhline(y=0, color='r', linestyle='--')
 plt.xlabel('Predicted Values')
 plt.ylabel('Residuals')
 plt.title('Residuals vs Predicted Values (Random Forest)')
 plt.show()
 
-# Actual vs Predicted for Random Forest
+# Atual vs Previsto para a Random Forest
 plt.figure(figsize=(10, 6))
-plt.scatter(y_test, y_pred_rf, alpha=0.5)
-plt.plot([y_test.min(), y_test.max()], [y_test.min(), y_test.max()], 'r--', lw=2)
+plt.scatter(YTest, PrevYRf, alpha=0.5)
+plt.plot([YTest.min(), YTest.max()], [YTest.min(), YTest.max()], 'r--', lw=2)
 plt.xlabel('Actual')
 plt.ylabel('Predicted')
 plt.title('Actual vs Predicted Values (Random Forest)')
 plt.show()
 
-# Distribution of Residuals for Random Forest
+# Distribuição de Residuos para a Random Forest
 plt.figure(figsize=(10, 6))
 sns.histplot(residuals_rf, kde=True, bins=30)
 plt.title('Distribution of Residuals (Random Forest)')
@@ -88,13 +89,13 @@ def plot_learning_curve(estimator, title, X, y, cv=None, n_jobs=None, train_size
     return plt
 
 
-# Learning curve for Linear Regression
-plot_learning_curve(lr_model, "Learning Curve (Linear Regression)", x_train, y_train, cv=5)
+# Curva de aprendizagem para a regressão linear
+plot_learning_curve(ModeloRl, "Learning Curve (Linear Regression)", XTrain, YTrain, cv=5)
 plt.show()
 
-# Learning curve for Random Forest
-plot_learning_curve(rf_model, "Learning Curve (Random Forest)", x_train, y_train, cv=5)
-plt.show()
+# Curva de aprendizagem para a Random Forest
+#plot_learning_curve(rf_model, "Learning Curve (Random Forest)", x_train, y_train, cv=5)
+#plt.show()
 
 # Correlation Heatmap
 plt.figure(figsize=(12, 8))
--- protocolo_teste.py
+++ protocolo_teste.py
@@ -6,59 +6,54 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import cross_val_score
 
 #Criação do protocolo de treino e avaliação
-
-final_df = pd.read_csv('normalized.csv', low_memory=False)
-x_train, x_test, y_train, y_test = train_test_split(final_df.drop(columns=['Price']), final_df['Price'], test_size=0.2, random_state=42)
+FinalDf = pd.read_csv('normalized.csv', low_memory=False)
+XTrain, XTest, YTrain, YTest = train_test_split(FinalDf.drop(columns=['Price']), FinalDf['Price'], test_size=0.2, random_state=42)
                                                     
-print("Tamanho do dataset: {0}".format(len(final_df)))                                                   
-print("Tamanho do dataset de treino: {0}".format(len(x_train)))
-print("Tamanho do dataset de teste: {0}".format(len(x_test)))
+print("Dataset size: {0}".format(len(FinalDf)))
+print("Training dataset size: {0}".format(len(XTrain)))
+print("Test dataset size: {0}".format(len(XTest)))
 
-# Train Linear Regression model
-lr_model = LinearRegression()
-lr_model.fit(x_train, y_train)
+# Treinar o modelo de regressão linear
+ModeloRl = LinearRegression()
+ModeloRl.fit(XTrain, YTrain)
 
-# Predict using the Linear Regression model
-y_pred_lr = lr_model.predict(x_test)
+# Previsão ao usar o modelo de regressão linear
+PrevYRl = ModeloRl.predict(XTest)
 
-# Evaluate Linear Regression model
-mse_lr = mean_squared_error(y_test, y_pred_lr)
-r2_lr = r2_score(y_test, y_pred_lr)
+# Avaliação do modelo de regressão linear
+RlMse = mean_squared_error(YTest, PrevYRl)
+AvalRl = r2_score(YTest, PrevYRl)
 
-print("Linear Regression - Mean Squared Error: ", mse_lr)
-print("Linear Regression - R2 Score: ", r2_lr)
+print("Linear Regression - Mean Squared Error: ", RlMse)
+print("Linear Regression - R2 Score: ", AvalRl)
 
+# Treinar o modelo Random Forest Regressor
+ModeloRf = RandomForestRegressor(random_state=42)
+ModeloRf.fit(XTrain, YTrain)
 
-# Train Random Forest Regressor model
-rf_model = RandomForestRegressor(random_state=42)
-rf_model.fit(x_train, y_train)
+# Previsão ao usar o modelo Random Forest Regressor
+PrevYRf = ModeloRf.predict(XTest)
 
-# Predict using the Random Forest Regressor model
-y_pred_rf = rf_model.predict(x_test)
+# Avaliação do modelo Random Forest Regressor
+RfMse = mean_squared_error(YTest, PrevYRf)
+AvalRf = r2_score(YTest, PrevYRf)
 
-# Evaluate Random Forest Regressor model
-mse_rf = mean_squared_error(y_test, y_pred_rf)
-r2_rf = r2_score(y_test, y_pred_rf)
-
-print("Random Forest Regressor - Mean Squared Error: ", mse_rf)
-print("Random Forest Regressor - R2 Score: ", r2_rf)
-
+print("Random Forest Regressor - Mean Squared Error: ", RfMse)
+print("Random Forest Regressor - R2 Score: ", AvalRf)
 
 # Mean Absolute Error
-mae_lr = mean_absolute_error(y_test, y_pred_lr)
-mae_rf = mean_absolute_error(y_test, y_pred_rf)
+MaeRl = mean_absolute_error(YTest, PrevYRl)
+MaeRf = mean_absolute_error(YTest, PrevYRf)
 
-print("Linear Regression - Mean Absolute Error: ", mae_lr)
-print("Random Forest Regressor - Mean Absolute Error: ", mae_rf)
-
-
-# Cross-Validation for Linear Regression
-cv_scores_lr = cross_val_score(lr_model, final_df.drop(columns=['Price']), final_df['Price'], cv=5, scoring='r2')
-print("Linear Regression - Cross-Validation R2 Scores: ", cv_scores_lr)
-print("Linear Regression - Average Cross-Validation R2 Score: ", cv_scores_lr.mean())
+print("Linear Regression - Mean Absolute Error: ", MaeRl)
+print("Random Forest Regressor - Mean Absolute Error: ", MaeRf)
 
 # Cross-Validation for Random Forest Regressor
-cv_scores_rf = cross_val_score(rf_model, final_df.drop(columns=['Price']), final_df['Price'], cv=5, scoring='r2')
+cv_scores_rf = cross_val_score(ModeloRf, FinalDf.drop(columns=['Price']), FinalDf['Price'], cv=5, scoring='r2')
 print("Random Forest Regressor - Cross-Validation R2 Scores: ", cv_scores_rf)
 print("Random Forest Regressor - Average Cross-Validation R2 Score: ", cv_scores_rf.mean())
 
+# Cross-Validation for Linear Regression
+cv_scores_lr = cross_val_score(ModeloRf, FinalDf.drop(columns=['Price']), FinalDf['Price'], cv=5, scoring='r2')
+print("Linear Regression - Cross-Validation R2 Scores: ", cv_scores_lr)
+print("Linear Regression - Average Cross-Validation R2 Score: ", cv_scores_lr.mean())
