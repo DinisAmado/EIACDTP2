a2fdc665bd1239c313ea289ff271d7ec1c96f2a3
DinisAmado <dinisrafaelamado@gmail.com>
2024-06-11 12:24:30 +0100

    alteração 1

--- correlacao.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Carregar os dados normalizados
-df_normalized = pd.read_csv('normalized.csv')
-
-# Calcular a correlação entre a coluna Price e as colunas Bathroom, Bedroom e YearBuilt
-correlation_matrix = df_normalized[['Price', 'Bathroom', 'Bedroom', 'Area']].corr()
-
-print(correlation_matrix)
--- graficos.py
+++ graficos.py
@@ -1,54 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-#Talvez este ficheiro não fique até ao fim do trabalho dependendo do pretendido
-
 # Carregar o CSV sem outliers
-df_together = pd.read_csv('csv_sem_outliers.csv')
+df_together = pd.read_csv('csv_sem_outliers.csv')  # certifique-se de que o caminho do arquivo está correto
 
+# Gráfico do preço em função dos quartos
+df_grouped_bedroom = df_together.groupby('Bedroom')['Price'].mean().reset_index()
 
-#Gráfico do preço em função dos quartos:
-
-# Agrupar os dados pelo número de quartos e calcular a média dos preços
-df_grouped = df_together.groupby('Bedroom')['Price'].mean().reset_index()
-
-# Criar o gráfico de barras
 fig, ax = plt.subplots(figsize=(10, 6))
-ax.bar(df_grouped['Bedroom'], df_grouped['Price'], color='skyblue')
+ax.bar(df_grouped_bedroom['Bedroom'], df_grouped_bedroom['Price'], color='skyblue')
 
-# Adicionar título e rótulos
 ax.set_title('Preço Médio em Função do Número de Quartos')
 ax.set_xlabel('Número de Quartos')
 ax.set_ylabel('Preço Médio')
 
-# Mostrar o gráfico
 plt.show()
 
+# Gráfico do preço em função das casas de banho
+df_grouped_bathroom = df_together.groupby('Bathroom')['Price'].mean().reset_index()
 
-#Gráfico para ver o preço em função das casas de banho:
-
-df_grouped = df_together.groupby('Bathroom')['Price'].mean().reset_index()
-
-# Criar o gráfico de barras
 fig, ax = plt.subplots(figsize=(10, 6))
-ax.bar(df_grouped['Bathroom'], df_grouped['Price'], color='skyblue')
+ax.bar(df_grouped_bathroom['Bathroom'], df_grouped_bathroom['Price'], color='skyblue')
 
-# Adicionar título e rótulos
 ax.set_title('Preço Médio em Função do Número de Casas de Banho')
-ax.set_xlabel('Número de Casas De Banho')
+ax.set_xlabel('Número de Casas de Banho')
 ax.set_ylabel('Preço Médio')
 
-# Mostrar o gráfico
 plt.show()
 
+# Gráfico do preço em função da área
+df_grouped_area = df_together.groupby('Area')['Price'].mean().reset_index()
 
-#Gráfico para ver o preço em função da área:
-
-df_grouped = df_together.groupby('Area')['Price'].mean().reset_index()
-
-# Criar o gráfico de barras
 fig, ax = plt.subplots(figsize=(10, 6))
-ax.bar(df_grouped['Area'], df_grouped['Price'], color='skyblue')
+ax.bar(df_grouped_area['Area'], df_grouped_area['Price'], color='skyblue')
 
-# Mostrar o gráfico
+ax.set_title('Preço Médio em Função da Área')
+ax.set_xlabel('Área')
+ax.set_ylabel('Preço Médio')
+
 plt.show()
